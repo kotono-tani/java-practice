6c7109eba87c09f2757589576f4a7002490c2e35
tanikotono <tanikotono@localhost>
2023-04-26 23:50:18 +0900

    課題1.10

---
+++ HelloWorld/src/sample/java1_10_2.java
@@ -0,0 +1,25 @@
+/*	
+問題.for文を使用して九九の計算を出力してください。
+	※出力例 	1×1=1 1×2=2…1×9=9
+				2×1=2 2×2=4…2×9=18
+						・
+						・
+						・
+				9×1=9 9×2=18…9×9=81
+				*/
+package sample;
+
+public class java1_10_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+    int num[] = {1,2,3,4,5,6,7,8,9};
+    for (int i = 0; i < num.length; i++) {
+    	for(int j = 0; j <num.length; j++) {
+    		int kuku = num[i] * num[j];
+    		System.out.println(num[i] + "×️" + num[j] + "=" + kuku);
+    	}
+    }
+	}
+
+}
