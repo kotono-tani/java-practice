313ea4804a3337b2d90f420ab90a1727672c64ec
tanikotono <tanikotono@localhost>
2023-04-27 18:08:12 +0900

    課題1.13

---
+++ HelloWorld/src/sample/java1_13_1.java
@@ -0,0 +1,33 @@
+/*
+	int型の配列aを作成してください。
+	配列aに1～5の数字を格納してください。
+	繰り返し処理を使用し、
+	配列aの数字を順番に表示させてください。
+
+	出力結果：1
+			  2
+			  3
+			  4
+			  5
+*/
+package sample;
+
+public class java1_13_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] a = new int[5];
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		a[4] = 5;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(a[i]);
+			System.out.print(" ");
+		}
+
+	}
+
+}
---
+++ HelloWorld/src/sample/java1_13_2.java
@@ -0,0 +1,36 @@
+/*
+	①配列aを作成し、1～5を格納してください。
+	
+	②参照型配列bを作成し、3を表示してください。
+		出力結果：3
+		
+	③配列a[3]を10で上書きし表示してください。
+		出力結果：10
+		
+	④配列aの要素数を「配列aの要素数は、○です。」という形で表示してください。
+		出力結果：配列aの要素数は、５です。
+*/
+package sample;
+
+public class java1_13_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+    //①
+    int[] a = new int [5];
+    int b = 2; //②
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    a[3] = 4;
+    a[4] = 5;
+    System.out.println(a[b]);
+    //③
+    a[3] = 10;
+    System.out.println(a[3]);
+    //④
+    String str= "配列aの要素数は、" + a.length + "です。";
+    System.out.println(str);
+	}
+
+}
