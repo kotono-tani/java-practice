1ebc89d7197076c67e652330f161f8e3b7941aea
tanikotono <tanikotono@localhost>
2023-05-01 21:54:55 +0900

    課題1_13_2修正再提出

--- HelloWorld/src/sample/java1_13_2.java
+++ HelloWorld/src/sample/java1_13_2.java
@@ -18,18 +18,30 @@ public class java1_13_2 {
 		// TODO 自動生成されたメソッド・スタブ
 		
 		// ①
+		// 格納
 		int[] a = {1,2,3,4,5};
+		
+		// 配列b作成
 		int[] b;
+		
+		// 条件
 		b = a; // ② 修正
 
+		// 出力
 		System.out.println(b[2]); //　修正
 		
 		// ③
+		// 上書き
 		a[3] = 10;
+		
+		// 出力
 		System.out.println(a[3]);
 		
 		// ④
+		// 代入
 		String str = "配列aの要素数は、" + a.length + "です。";
+		
+		// 出力
 		System.out.println(str);
 	}
 
