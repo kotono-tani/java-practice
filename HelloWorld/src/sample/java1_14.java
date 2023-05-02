4aaff681dc0d26bd5d8d365ebb8a768fc68eb777
tanikotono <tanikotono@localhost>
2023-05-02 23:50:28 +0900

    課題1_14

--- HelloWorld/src/sample/java1_14.java
+++ HelloWorld/src/sample/java1_14.java
@@ -24,30 +24,39 @@ public class java1_14 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+
 		// a~dまで表示
 		String[] language = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" }; //格納
 		for (int i = 0; i < 10; i++) { // 繰り返し処理
+
+			// switch文
 			switch (language[i]) {
-			case "a":
-			case "b":
-			case "c":
-			case "d":
+			case "a": // a代入
+			case "b": // b代入
+			case "c": // c代入
+			case "d": // d代入
+
 				System.out.println(language[i]); // 出力
-				System.out.print(" ");
+
+				System.out.print(" "); // 改行
+
 				break; // dで止める
 			}
 
 		}
 		System.out.println(""); // 改行
-		
+
 		// i意外表示
 		String[] name = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" }; // 格納
+
 		for (int a = 0; a < 10; a++) { // 繰り返し
+
 			if (a == 8) { // i以外
 				continue;
 			}
 			System.out.println(name[a]); // 出力
-			System.out.print(" ");
+
+			System.out.print(" "); // 改行
 		}
 
 	}
