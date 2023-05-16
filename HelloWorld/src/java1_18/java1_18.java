1cf66df85a5e5e12af09fadd2650ce3a401c42db
tanikotono <tanikotono@localhost>
2023-05-17 00:12:46 +0900

    課題1.18再提出

--- HelloWorld/src/java1_18/java1_18.java
+++ HelloWorld/src/java1_18/java1_18.java
@@ -130,8 +130,8 @@ public class java1_18 {
 		// Q4
 		mul(3.14);
 
-		// Q5 // 修正
-		int[] numbers = generateRandomNumbers(5);
+		// Q5 
+		int[] numbers = generateRandomNumbers(5); // 修正
 
 		// Q6 
 		calculateAverage(numbers); // 修正
